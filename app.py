743e7002a35c69e01eb69cabce713a3c6b192676
shun ishii <shun_ishii@shunnoMacBook-puro.local>
2024-11-02 14:02:57 +0900

    2024-11-02

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 from flask import Flask, render_template, Response
-import cv2
+from PIL import Image
+import io
+import cv2  # カメラのキャプチャには依然としてOpenCVが必要です
 
 app = Flask(__name__)
 
@@ -11,14 +13,21 @@ def generate_frames():
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+            # OpenCVの画像をPillowで処理する
+            image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            buffer = io.BytesIO()
+            image.save(buffer, format="JPEG")
+            frame = buffer.getvalue()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/viewer1')
 def viewer1():
@@ -27,10 +36,6 @@ def viewer1():
 @app.route('/viewer2')
 def viewer2():
     return render_template('viewer2.html')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)

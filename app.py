8ffa080f336512506e2b0dbf60ee03294af1b667
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-11-07 11:25:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, Response
+from flask_socketio import SocketIO, emit
+import json
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+# 接続中のクライアント管理
+clients = {}
 
 @app.route('/')
 def index():
@@ -14,5 +21,30 @@ def viewer1():
 def viewer2():
     return render_template('viewer2.html')
 
+@socketio.on('connect')
+def handle_connect():
+    clients[request.sid] = {'type': None}
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    if request.sid in clients:
+        del clients[request.sid]
+
+@socketio.on('register')
+def handle_register(data):
+    clients[request.sid]['type'] = data['type']
+
+@socketio.on('offer')
+def handle_offer(data):
+    emit('offer', data, broadcast=True, skip_sid=request.sid)
+
+@socketio.on('answer')
+def handle_answer(data):
+    emit('answer', data, broadcast=True, skip_sid=request.sid)
+
+@socketio.on('ice_candidate')
+def handle_ice_candidate(data):
+    emit('ice_candidate', data, broadcast=True, skip_sid=request.sid)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)

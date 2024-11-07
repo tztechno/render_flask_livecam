deb2c18c62b994e5e3e1d6826297eff2647e472e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-11-07 11:56:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
-import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 # 接続中のクライアント管理
 clients = {}
@@ -24,27 +23,33 @@ def viewer2():
 @socketio.on('connect')
 def handle_connect():
     clients[request.sid] = {'type': None}
+    print(f'Client connected: {request.sid}')
 
 @socketio.on('disconnect')
 def handle_disconnect():
     if request.sid in clients:
         del clients[request.sid]
+        print(f'Client disconnected: {request.sid}')
 
 @socketio.on('register')
 def handle_register(data):
     clients[request.sid]['type'] = data['type']
+    print(f'Client {request.sid} registered as {data["type"]}')
 
 @socketio.on('offer')
 def handle_offer(data):
     emit('offer', data, broadcast=True, skip_sid=request.sid)
+    print(f'Offer received and broadcasted from {request.sid}')
 
 @socketio.on('answer')
 def handle_answer(data):
     emit('answer', data, broadcast=True, skip_sid=request.sid)
+    print(f'Answer received and broadcasted from {request.sid}')
 
 @socketio.on('ice_candidate')
 def handle_ice_candidate(data):
     emit('ice_candidate', data, broadcast=True, skip_sid=request.sid)
+    print(f'ICE candidate received and broadcasted from {request.sid}')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)

a2370d12c4a57685df8a085f9bc2345850e6a9ce
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-11-07 12:11:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*")
+
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 # 接続中のクライアント管理
 clients = {}

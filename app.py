ab65ab61054528a7bba6d9e2e75d0560637c370b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-11-07 11:10:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import cv2
 app = Flask(__name__)
 
 def generate_frames():
-    camera = cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(1)
     while True:
         success, frame = camera.read()
         if not success:

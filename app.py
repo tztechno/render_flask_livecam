67d81523094017d9da79e9cc2b88a547466f7926
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-11-07 11:16:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
 from flask import Flask, render_template, Response
-import cv2
 
 app = Flask(__name__)
-
-def generate_frames():
-    camera = cv2.VideoCapture(1)
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
@@ -26,10 +13,6 @@ def viewer1():
 @app.route('/viewer2')
 def viewer2():
     return render_template('viewer2.html')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
